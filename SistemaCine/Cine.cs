ad0202b80e488ed1d231030f3f2a12a657f73b58
Luis Martin Peña Mejia <luismartinpm2580@gmail.com>
2026-01-17 13:51:57 -0400

    Agregar archivos de proyecto.

---
+++ SistemaCine/Cine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaCine
+{
+    public class Cine
+    {
+        private int contarpersonas;
+        private int capacidadmaxima;
+        private int entradasVendidas;
+        private int precioEntrada = 100;
+
+        public Cine(int capacidad)
+        {
+            this.capacidadmaxima = capacidad;
+            this.contarpersonas = 0;
+            this.entradasVendidas = 0;
+        }
+        public void venderTaquilla()
+        {
+            if (contarpersonas < capacidadmaxima)
+            {
+                contarpersonas++;      
+                entradasVendidas++;   
+                Console.WriteLine($"Venta realizada. Asiento ocupado. Precio: RD${precioEntrada}");
+            }
+            else
+            {
+                Console.WriteLine("¡La sala está llena! No se puede vender más.");
+            }
+        }
+
+        public void verAforo()
+        {
+            int libres = capacidadmaxima - contarpersonas;
+
+            double porcentaje = ((double)contarpersonas / capacidadmaxima) * 100;
+
+            Console.WriteLine($"Estado del Cine");
+            Console.WriteLine($"Asientos Usados: {contarpersonas} personas");
+            Console.WriteLine($"Asientos Disponibles: {libres} asientos");
+            Console.WriteLine($"Estado de sala: {porcentaje:F1}%"); 
+            Console.WriteLine($"Total vendido: RD${entradasVendidas * precioEntrada}");
+        }
+
+        public void sacarPersonas()
+        {
+            if (contarpersonas > 0)
+            {
+                contarpersonas--; 
+                             
+                Console.WriteLine("Una persona ha salido de la sala.");
+            }
+            else
+            {
+                Console.WriteLine("La sala ya está vacía.");
+            }
+        }
+
+    }
+}
---
+++ SistemaCine/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SistemaCine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Cine miCine = new Cine(50);
+
+            bool continuar = true;
+
+            while (continuar)
+            {
+                Console.Clear(); 
+                Console.WriteLine("BIENVENIDO A CINEMAX UCE"); 
+
+                Console.WriteLine("1. Vender Taquilla");
+                Console.WriteLine("2. Ver Aforo (Estado de la sala)");
+                Console.WriteLine("3. Sacar Persona (Salida)");
+                Console.WriteLine("4. Salir del Sistema");
+
+                Console.Write("Seleccione una opción: ");
+
+                string opcion = Console.ReadLine();
+
+                Console.WriteLine(); 
+
+                switch (opcion)
+                {
+                    case "1":
+                        miCine.venderTaquilla();
+                        break;
+
+                    case "2":
+                        miCine.verAforo();
+                        break;
+
+                    case "3":
+                        miCine.sacarPersonas();
+                        break;
+
+                    case "4":
+                        continuar = false;
+                        Console.WriteLine("Cerrando el sistema...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción no válida. Intente de nuevo.");
+                        break;
+                }
+
+                if (continuar)
+                {
+                    Console.WriteLine("\nPresione cualquier tecla para continuar...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
